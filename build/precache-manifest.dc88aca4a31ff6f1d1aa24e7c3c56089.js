323410d229653f10d4e27045751f02fc283ec7f6
SamiraMim <59778116+SamiraMim@users.noreply.github.com>
2020-09-05 02:41:23 +0430

    Add files via upload

---
+++ build/precache-manifest.dc88aca4a31ff6f1d1aa24e7c3c56089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e71dec8d4383efb0a162223ceea8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f230480cf8de7661e04",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "676181928769ea06857a",
+    "url": "/static/js/2.569193bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.569193bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f230480cf8de7661e04",
+    "url": "/static/js/main.a8b65cab.chunk.js"
+  },
+  {
+    "revision": "5d8e586e629a3c8fc65c",
+    "url": "/static/js/runtime-main.fdfd1f02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
